a5dd94b7e022c6e6ee9359145a1b1c0675aad523
Anna Savina <anna.s.savina@gmail.com>
2017-12-08 23:26:43 +0300

    Передача параметров в метод действий, получение параметров из контекста

--- MetanitMvc5/Controllers/HomeController.cs
+++ MetanitMvc5/Controllers/HomeController.cs
@@ -8,13 +8,24 @@ namespace MetanitMvc5.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-
         public ActionResult Index()
         {
             return View();
         }
 
+        public string TriangleSquare(int a, int h)
+        {
+            double s = a * h / 2.0;
+            return "<h2>Площадь треугольника с основанием " + a +
+                    " и высотой " + h + " равна " + s + "</h2>";
+        }
+
+        public string Square()
+        {
+            int a = Int32.Parse(Request.Params["a"]);
+            int b = Int32.Parse(Request.Params["b"]);
+            int s = a * b;
+            return "<h2>Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + s + "</h2>";
+        }
     }
 }
