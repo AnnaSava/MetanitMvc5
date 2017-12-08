e8802660af84df0992b0bb899a166cb6a7900207
Anna Savina <anna.s.savina@gmail.com>
2017-12-09 01:12:31 +0300

    Встроенные результаты действий: статусы запроса и файлы

--- MetanitMvc5/Controllers/HomeController.cs
+++ MetanitMvc5/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using MetanitMvc5.Util;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -38,6 +39,65 @@ namespace MetanitMvc5.Controllers
         {
             string path = "../Images/panda.png";
             return new ImageResult(path);
+        }
+
+        public ActionResult StatusCode404(int age)
+        {
+            if (age < 21)
+            {
+                return new HttpStatusCodeResult(404);
+            }
+            return View();
+        }
+
+        public ActionResult NotFound(int age)
+        {
+            if (age < 21)
+            {
+                return HttpNotFound();
+            }
+            return View();
+        }
+
+        public ActionResult Unauthorized(int age)
+        {
+            if (age < 21)
+            {
+                return new HttpUnauthorizedResult();
+            }
+            return View();
+        }
+
+        public FileResult GetFile()
+        {
+            // Путь к файлу
+            string file_path = Server.MapPath("~/Images/panda.png");
+            // Тип файла - content-type
+            string file_type = "image/png";
+            // Имя файла - необязательно
+            string file_name = "panda.png";
+            return File(file_path, file_type, file_name);
+        }
+
+        // Отправка массива байтов
+        public FileResult GetBytes()
+        {
+            string path = Server.MapPath("~/Images/panda.png");
+            byte[] mas = System.IO.File.ReadAllBytes(path);
+            string file_type = "image/png";
+            string file_name = "panda.png";
+            return File(mas, file_type, file_name);
+        }
+
+        // Отправка потока
+        public FileResult GetStream()
+        {
+            string path = Server.MapPath("~/Images/panda.png");
+            // Объект Stream
+            FileStream fs = new FileStream(path, FileMode.Open);
+            string file_type = "image/png";
+            string file_name = "panda.png";
+            return File(fs, file_type, file_name);
         }
     }
 }
